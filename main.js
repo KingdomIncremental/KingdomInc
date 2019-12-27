62dd6c186bfa514c830c62b179460c062666b63d
Kris J <KrisJohnson458@gmail.com>
2019-12-26 23:58:52 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//Initial game vars
+var food = 0;
+var wood = 0; 
+var totalCitizens = 0;
+var availableCitizens = 0;
+
+var foodLoss = 0; 
+
+var incrementLevel = 1;
+
+
+var foodGatherers = 0;
+var woodGatherers = 0;
+
+function assignFoodGatherer(incrementLevel)
+{
+    if(availableCitizens >= incrementLevel)
+    {
+        foodGatherers += incrementLevel;
+        availableCitizens -= incrementLevel;
+    }
+    
+}
+
+function assignWoodGatherer(incrementLevel)
+{
+    if(availableCitizens >= incrementLevel)
+    {
+        woodGatherers += incrementLevel;
+        availableCitizens -= incrementLevel;
+    }
+}
+
